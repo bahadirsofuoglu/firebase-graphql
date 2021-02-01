49ad1ccdd1100aa2d4b290f8f4c1d8830ac53ae7
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2021-02-01 14:55:02 +0300

    ready for development

---
+++ app.js
@@ -0,0 +1,21 @@
+const { ApolloServer } = require('apollo-server')
+
+const fs = require('fs')
+const path = require('path')
+
+// Import typeDefs and resolvers
+const filePath = path.join(__dirname, 'graphql', 'typeDefs.gql')
+const typeDefs = fs.readFileSync(filePath, 'utf-8')
+const resolvers = require('./graphql/resolvers')
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  formatError: error => ({
+    name: error.name,
+    message: error.message.replace('Context creation failed:', '')
+  })
+})
+server.listen().then(({ url }) => {
+  console.log(`Server listening on ${url}`)
+})
