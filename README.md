37c03b92e8356961d13dfcb43e9b4a0989a7ee73
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2021-02-03 16:38:34 +0300

    readme

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-
 # firebase-graphql
+This project answers the following questions. 
+
+- [How are resolver, typedefs filed ?]
+- [How to use firestore with node.js?]
+- [How to work with relation data in graphql ?]
+
+#### Dotenv for firebase
+```sh
+API_KEY=
+AUTH_DOMAIN=
+PROJECT_ID=
+STORAGE_BUCKET=
+MESSAGING_SENDER_ID=
+APP_ID=
+MEASUREMENT_ID=
+```
--- app.js
+++ app.js
@@ -1,12 +1,4 @@
 const { ApolloServer } = require('apollo-server')
-
-/* const fs = require('fs')
-const path = require('path')
-
-// Import typeDefs and resolvers
-const filePath = path.join(__dirname, 'schemas', 'typeDefs.gql')
-const typeDefs = fs.readFileSync(filePath, 'utf-8') */
-
 const typeDefs = require('./schemas')
 const resolvers = require('./resolvers')
 
